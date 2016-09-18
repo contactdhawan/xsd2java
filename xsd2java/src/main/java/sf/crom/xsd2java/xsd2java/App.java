2b65ecf8f26dea3efdff85fda93521a1aaf89df1
contactdhawan <contactdhawan@gmail.com>
2016-09-18 00:09:18 -0500

    convert a xsd to java

---
+++ xsd2java/src/main/java/sf/crom/xsd2java/xsd2java/App.java
@@ -0,0 +1,13 @@
+package sf.crom.xsd2java.xsd2java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
